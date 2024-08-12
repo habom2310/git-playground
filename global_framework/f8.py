81a3316ed89cec4f4d9437230c06e5f9d64d6c2d
Ha Nguyen <habom2310@gmail.com>
2024-08-13 09:22:27 +1000

    merge nonprod to main

---
+++ global_framework/f8.py
@@ -0,0 +1,72 @@
+# this is a very long code
+
+import abc123
+import abc123
+import abc123
+import abc123
+import abc123
+import abc123
+import abc123
+import abc123
+import abc123
+import abc123
+
+## hello
+
+x = """
+asdnvkasdva
+sdva
+sadv
+asdvasdv
+sadvas
+dvasvd
+asdv
+asdv
+asdv
+"""
+
+y = """
+asdkva
+v
+sdaklvn
+dfgkn
+dsfkn
+asfd
+sdfakl
+fsdajkn
+sfadjkn
+dfsajk
+sfdakln
+asdj
+"""
+
+z = """
+anvqiewr
+qwer
+qwer
+qwer
+qwrqw
+erqwer
+qwtwe
+rretu
+rtyiytu
+htgxf
+bcvh
+tryr
+arsezfgdHDyrest
+arsf
+dtfhg
+dasr
+eytw
+hdfjgfhdsaf
+rtdyure
+wryjhgfx
+dzgsftrytduys
+earghgn
+xhfdrsgtaystud
+yetgfhgn
+xfhtysret
+awsgfdhxg
+zg
+df
+"""
